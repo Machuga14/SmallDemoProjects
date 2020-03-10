abe66aaa4ea792bcb937ffac29669843122a76d6
Matthew <alpineskiers@comcast.net>
2020-03-10 11:40:18 -0600

    Phase 1 GUI Practices for a friend.

---
+++ CSharpGUIPractices/Phase 1/CSharpGUIPractices/GraduateStudent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGUIPractices
+{
+  public class GraduateStudent : Student
+  {
+    public GraduateStudent()
+      : base()
+    {
+    }
+
+    public string Emphasis { get; set; }
+
+    public override StudentType TypeOfStudent
+    {
+      get
+      {
+        return StudentType.GraduateStudent;
+      }
+    }
+  }
+}
---
+++ CSharpGUIPractices/Phase 1/CSharpGUIPractices/StudentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGUIPractices
+{
+  [Flags]
+  public enum StudentType : ulong
+  {
+    NormalStudent = 1,
+
+    GraduateStudent = 1024
+  }
+}
